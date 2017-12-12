4f47a10a5129198aefebb6874c38906c75987aa8
Nolan <nolanruss@gmail.com>
2017-12-12 00:05:30 -0800

    App update, package.json

---
+++ src/components/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">{{this.$data.title}}</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view />
+    <div class="container" >
+      <h3>{{this.$data.title}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Visitor Sign In'
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #363636;
+  margin-top: 0px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+a {
+  color: #363636;
+}
+.btn {
+  border: 0;
+  background: #860038;
+  color: white;
+  outline: 0;
+}
+.btn:active {
+  background: #860038;
+  outline: 0;
+}
+.btn:focus {
+  background: #860038;
+  outline: 0;
+}
+.btn:active:focus {
+  background: #860038;
+  outline: 0;
+}
+.btn-default {
+  border: 0;
+  background: #860038;
+  color: white;
+}
+.btn-primary {
+  border: 0;
+  background: #860038;
+  color: white;
+}
+.btn-primary:hover {
+  background: #EFBA08;
+  color: white;
+}
+</style>
