8e936a749975705e6f62bc0bc0aa48e83235ffc1
Nolan <nolanruss@gmail.com>
2017-12-12 16:28:20 -0800

    Updated Client Table registration

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import VueRouter from 'vue-router'
 import {ServerTable, ClientTable, Event} from 'vue-tables-2'
 import store from './store'
 
+// Register Vue add-ons
 Vue.use(VeeValidate)
 Vue.use(VueRouter)
-Vue.use(ClientTable, {}, false, )
+Vue.use(ClientTable, {
+  compileTemplates: true,
+  filterByColumn: true,
+  texts: {
+    filter: "Search:"
+  }
+})
+
 import App from './components/App.vue'
 import Signin from './components/Signin.vue'
 import Admin from './components/Admin.vue'
